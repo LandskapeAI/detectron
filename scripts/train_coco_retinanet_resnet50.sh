0c56763d30de49db536107f985a77cc69fe8ac39
iyaja <ajayuppili@gmail.com>
2020-07-02 19:41:59 -0400

    fix(scripts): change python script names

--- scripts/train_coco_retinanet_resnet50.sh
+++ scripts/train_coco_retinanet_resnet50.sh
@@ -1,4 +1,4 @@
 export DETECTRON2_DATASETS=~/datasets
-python train.py --num-gpus 4 \
+python main.py --num-gpus 4 \
         --config-file ./detectron_configs/COCO-Detection/retinanet_resnet50_fpn_1x.yaml \
 	#--eval-only MODEL.WEIGHTS ./output/model_final.pth
